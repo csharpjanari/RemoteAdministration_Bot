ddbedef29567ea8ed519000fc512ab0863a2784f
Raul Janari <rdjanari1707@gmail.com>
2022-11-04 16:57:55 +0100

    Methods for Shutdown & Reboot PC

--- RemoteAdministartion/Handlers/UpdateHandlerAsync.cs
+++ RemoteAdministartion/Handlers/UpdateHandlerAsync.cs
@@ -1,5 +1,8 @@
 ﻿using Telegram.Bot.Types;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Diagnostics;
 
 namespace RemoteAdministration_Bot.Handlers
 {
@@ -7,7 +10,67 @@ namespace RemoteAdministration_Bot.Handlers
     {
         public static async Task Handler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            
+            var handler = update.Type switch
+            {
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message!),
+                _ => UnknownUpdateHandler(botClient, update)
+            };
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            Console.WriteLine($"{message.Chat.Username ?? "Anonymus"}   |   {message.Text}");
+
+            if (message.Type != MessageType.Text)
+                return;
+
+            var action = message.Text!.Split(' ')[0] switch
+            {
+                "/Shutdown" => ShutdownPC(),
+                "/Reboot" => RebootPC(),
+                _ => Usage(botClient, message)
+            };
+
+            Console.WriteLine($"The message was sent with id: {message.MessageId}");
+
+
+
+            static Task ShutdownPC()
+            {
+                Process.Start("cmd", "/c shutdown -s -f -t 00");
+
+                return Task.CompletedTask;
+            }
+
+
+
+            static Task RebootPC()
+            {
+                Process.Start("shutdown", "-r -f -t 00");
+
+                return Task.CompletedTask;
+            }
+        }        
+
+
+
+        static async Task<Message> Usage(ITelegramBotClient botClient, Message message)
+        {
+            const string usage = "Select one of the operations : \n" +
+                                 "/Shutdown - shutting down the PC \n" +
+                                 "/Reboot - rebooting PC";
+
+            return await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                                                        text: usage,
+                                                        replyMarkup: new ReplyKeyboardRemove());
+        }
+
+
+
+        private static Task UnknownUpdateHandler(ITelegramBotClient botClient, Update update)
+        {
+            Console.WriteLine($"Unknown update type: {update.Type}");
+            return Task.CompletedTask;
         }
     }
 }
